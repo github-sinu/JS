f689c782893c05ccd5ec5637a211803ec5274fb0
github-sinu <57267811+github-sinu@users.noreply.github.com>
2024-02-03 07:34:44 +0000

    Datatypes

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "sonali@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@gamail.com"
+accountPassword = "25225"
+accountCity = "jaipur"
+let accountState;
+
+console.log(accountId);
+
+/*
+
+Prefer not to use var
+because of issues in block scode and functional scope
+
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountPassword, accountState])
