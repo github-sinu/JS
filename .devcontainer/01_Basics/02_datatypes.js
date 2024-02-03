ab92d7c7a1fa473c637f5df446d154a2dcad2eaf
github-sinu <57267811+github-sinu@users.noreply.github.com>
2024-02-03 07:52:59 +0000

    conversion of datatypes

---
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3 ) // we are using node js not browser
+
+console.log(3+
+    
+    3) // code readability shold be high
+
+    
+
+// number => 2 to power 53
+// bigint
+// boolean => true/false
+// string => " "
+// null => standalone value (type - object)
+// undefined =>
+// symbol =>unique
+
+// object
+
+console.log(typeof undefined);
